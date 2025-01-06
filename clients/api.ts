53ce56bc9d511309a6e49f81d95f5be2a26a5743
sri <chegondisrianjaneyulu@gmail.com>
2025-01-06 10:18:35 +0530

    update

--- clients/api.ts
+++ clients/api.ts
@@ -3,7 +3,7 @@ import { GraphQLClient } from 'graphql-request'
 
 const isClient = typeof window !== "undefined"
 
-export const graphqlClient = new GraphQLClient(process.env.NEXT_PUBLIC_API_URL as string, {
+export const graphqlClient = new GraphQLClient("https://sri.work.gd/graphql", {
     headers: () => ({
         Authorization: isClient ? `Bearer ${window.localStorage.getItem('__twitter_token')}` : ""
     }),
